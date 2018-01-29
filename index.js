0e2fc44d3d76a0f8b4aebf46af3d18c33379bda4
hobbyquaker <bastiraff@googlemail.com>
2018-01-29 21:04:13 +0100

    code style

--- index.js
+++ index.js
@@ -1,200 +1,202 @@
-'use strict'
+/* eslint-disable camelcase */
 
-const url = require('url')
-const WebSocket = require('ws')
-const EventEmitter = require('events')
-const rp = require('request-promise')
+const url = require('url');
+const EventEmitter = require('events');
+const WebSocket = require('ws');
+const rp = require('request-promise');
 
 module.exports = class UnifiEvents extends EventEmitter {
 
-  constructor (opts) {
-    super()
+    constructor(opts) {
+        super();
 
-    this.opts = opts
-    this.opts.site = this.opts.site || 'default'
-    this.userAgent = 'UniFi Events'
-    this.controller = url.parse(this.opts.controller)
-    this.jar = rp.jar()
-    this.rp = rp.defaults({
-      rejectUnauthorized: this.opts.rejectUnauthorized,
-      jar: this.jar,
-      headers: {
-        'User-Agent': this.userAgent
-      }
-    })
+        this.opts = opts;
+        this.opts.site = this.opts.site || 'default';
+        this.userAgent = 'UniFi Events';
+        this.controller = url.parse(this.opts.controller);
+        this.jar = rp.jar();
+        this.rp = rp.defaults({
+            rejectUnauthorized: this.opts.rejectUnauthorized,
+            jar: this.jar,
+            headers: {
+                'User-Agent': this.userAgent
+            }
+        });
 
-    this.autoReconnectInterval = 5 * 1000
+        this.autoReconnectInterval = 5 * 1000;
 
-    // login and start listening
-    if (this.opts.listen !== false) {
-      this.connect()
+        // Login and start listening
+        if (this.opts.listen !== false) {
+            this.connect();
+        }
+
+        // Convenience emitters
+        this.helpers = {
+            EVT_WU_Connected: 'connected',
+            EVT_WU_Disconnected: 'disconnected',
+            EVT_WG_Connected: 'connected',
+            EVT_WG_Disconnected: 'disconnected',
+            EVT_LU_CONNECTED: 'connected',
+            EVT_LU_DISCONNECTED: 'disconnected',
+            EVT_LG_CONNECTED: 'connected',
+            EVT_LG_DISCONNECTED: 'disconnected'
+        };
     }
 
-    // convenience emitters
-    this.helpers = {
-      'EVT_WU_Connected': 'connected',
-      'EVT_WU_Disconnected': 'disconnected',
-      'EVT_WG_Connected': 'connected',
-      'EVT_WG_Disconnected': 'disconnected',
-      'EVT_LU_CONNECTED': 'connected',
-      'EVT_LU_DISCONNECTED': 'disconnected',
-      'EVT_LG_CONNECTED': 'connected',
-      'EVT_LG_DISCONNECTED': 'disconnected'
-    }
-  }
-
-  connect (reconnect) {
-    return this._login(reconnect)
+    connect(reconnect) {
+        return this._login(reconnect)
       .then(() => {
-        return this._listen()
-      })
-  }
+          return this._listen();
+      });
+    }
 
-  _login (reconnect) {
-    return this.rp.post(`${this.controller.href}api/login`, {
-      resolveWithFullResponse: true,
-      json: {
-        username: this.opts.username,
-        password: this.opts.password,
-        strict: true
-      }
-    })
+    _login(reconnect) {
+        return this.rp.post(`${this.controller.href}api/login`, {
+            resolveWithFullResponse: true,
+            json: {
+                username: this.opts.username,
+                password: this.opts.password,
+                strict: true
+            }
+        })
     .catch(() => {
-      if (!reconnect) {
-        this._reconnect()
-      }
-    })
-  }
+        if (!reconnect) {
+            this._reconnect();
+        }
+    });
+    }
 
-  _listen () {
-    let cookies = this.jar.getCookieString(this.controller.href)
-    const ws = new WebSocket(`wss://${this.controller.host}/wss/s/${this.opts.site}/events`, {
-      perMessageDeflate: false,
-      rejectUnauthorized: this.opts.rejectUnauthorized,
-      headers: {
-        'User-Agent': this.userAgent,
-        'Cookie': cookies
-      }
-    })
+    _listen() {
+        const cookies = this.jar.getCookieString(this.controller.href);
+        const ws = new WebSocket(`wss://${this.controller.host}/wss/s/${this.opts.site}/events`, {
+            perMessageDeflate: false,
+            rejectUnauthorized: this.opts.rejectUnauthorized,
+            headers: {
+                'User-Agent': this.userAgent,
+                Cookie: cookies
+            }
+        });
 
     // Ping the server every 15 seconds to keep the connection alive.
-    let pingpong = setInterval(() => {
-      ws.send('ping')
-    }, 15000)
+        const pingpong = setInterval(() => {
+            ws.send('ping');
+        }, 15000);
 
-    ws.on('open', () => {
-      this.reconnecting = false
-      this.emit('ready')
-      this.emit('websocket-status', `UniFi Events: Connected to ${this.opts.controller}`)
-    })
+        ws.on('open', () => {
+            this.reconnecting = false;
+            this.emit('ready');
+            this.emit('websocket-status', `UniFi Events: Connected to ${this.opts.controller}`);
+        });
 
-    ws.on('message', (data, flags) => {
-      if (data === 'pong') { return }
-      try {
-        let parsed = JSON.parse(data)
-        if ('data' in parsed && Array.isArray(parsed.data)) {
-          parsed.data.forEach((entry) => {
-            this._event(entry)
-          })
+        ws.on('message', data => {
+            if (data === 'pong') {
+                return;
+            }
+            try {
+                const parsed = JSON.parse(data);
+                if ('data' in parsed && Array.isArray(parsed.data)) {
+                    parsed.data.forEach(entry => {
+                        this._event(entry);
+                    });
+                }
+            } catch (err) {
+                this.emit('websocket-status', `UniFi Events: Failed to parse message.`);
+            }
+        });
+
+        ws.on('close', e => {
+            clearInterval(pingpong);
+            this._reconnect(e);
+        });
+
+        ws.on('error', e => {
+            clearInterval(pingpong);
+            this.emit('websocket-status', `UniFi Events: error - ${e.message}`);
+            this._reconnect(e);
+        });
+    }
+
+    _reconnect() {
+        if (!this.reconnecting) {
+            this.emit('websocket-status', `UniFi Events: disconnected - retry in ${this.autoReconnectInterval}ms`);
+            this.reconnecting = true;
+            setTimeout(() => {
+                this.emit('websocket-status', 'UniFi Events: reconnecting...');
+                this.reconnecting = false;
+                this.connect(true);
+            }, this.autoReconnectInterval);
         }
-      } catch (e) {
-        this.emit('websocket-status', `UniFi Events: Failed to parse message.`)
-      }
-    })
-
-    ws.on('close', (e) => {
-      clearInterval(pingpong)
-      this._reconnect(e)
-    })
-
-    ws.on('error', (e) => {
-      clearInterval(pingpong)
-      this.emit('websocket-status', `UniFi Events: error - ${e.message}`)
-      this._reconnect(e)
-    })
-  }
-
-  _reconnect (e) {
-    if (!this.reconnecting) {
-      this.emit('websocket-status', `UniFi Events: disconnected - retry in ${this.autoReconnectInterval}ms`)
-      this.reconnecting = true
-      setTimeout(() => {
-        this.emit('websocket-status', 'UniFi Events: reconnecting...')
-        this.reconnecting = false
-        this.connect(true)
-      }, this.autoReconnectInterval)
     }
-  }
 
-  _event (data) {
-    this.emit(data.key, data)
-    this.emit('event', data)
+    _event(data) {
+        this.emit(data.key, data);
+        this.emit('event', data);
 
-    // send to convenience emitters
-    if (data.key in this.helpers) {
-      this.emit(this.helpers[data.key], data)
+    // Send to convenience emitters
+        if (data.key in this.helpers) {
+            this.emit(this.helpers[data.key], data);
+        }
     }
-  }
 
-  _ensureLoggedIn () {
-    return this.rp.get(`${this.controller.href}api/self`)
+    _ensureLoggedIn() {
+        return this.rp.get(`${this.controller.href}api/self`)
       .catch(() => {
-        return this._login()
-      })
-  }
+          return this._login();
+      });
+    }
 
-  getApi (path) {
-    return this._ensureLoggedIn()
+    getApi(path) {
+        return this._ensureLoggedIn()
       .then(() => {
-        return this.rp.get(`${this.controller.href}api/s/${this.opts.site}/${path}`, {
-          json: true
-        })
-      })
-  }
+          return this.rp.get(`${this.controller.href}api/s/${this.opts.site}/${path}`, {
+              json: true
+          });
+      });
+    }
 
-  delApi (path) {
-    return this._ensureLoggedIn()
+    delApi(path) {
+        return this._ensureLoggedIn()
       .then(() => {
-        return this.rp.del(`${this.controller.href}api/s/${this.opts.site}/${path}`, {
-          json: true
-        })
-      })
-  }
+          return this.rp.del(`${this.controller.href}api/s/${this.opts.site}/${path}`, {
+              json: true
+          });
+      });
+    }
 
-  postApi (path, body) {
-    return this._ensureLoggedIn()
+    postApi(path, body) {
+        return this._ensureLoggedIn()
       .then(() => {
-        return this.rp.post(`${this.controller.href}api/s/${this.opts.site}/${path}`, {
-          body,
-          json: true
-        })
-      })
-  }
+          return this.rp.post(`${this.controller.href}api/s/${this.opts.site}/${path}`, {
+              body,
+              json: true
+          });
+      });
+    }
 
-  getClients () {
-    return this.getApi('stat/sta')
-  }
+    getClients() {
+        return this.getApi('stat/sta');
+    }
 
-  getClient (mac) {
-    return this.getApi(`stat/user/${mac}`)
-  }
+    getClient(mac) {
+        return this.getApi(`stat/user/${mac}`);
+    }
 
-  getSites () {
-    return this._ensureLoggedIn()
+    getSites() {
+        return this._ensureLoggedIn()
       .then(() => {
-        return this.rp.get(`${this.controller.href}api/self/sites`, {
-          json: true
-        })
-      })
-  }
+          return this.rp.get(`${this.controller.href}api/self/sites`, {
+              json: true
+          });
+      });
+    }
 
-  getSitesStats () {
-    return this._ensureLoggedIn()
+    getSitesStats() {
+        return this._ensureLoggedIn()
       .then(() => {
-        return this.rp.get(`${this.controller.href}api/stat/sites`, {
-          json: true
-        })
-      })
-  }
+          return this.rp.get(`${this.controller.href}api/stat/sites`, {
+              json: true
+          });
+      });
+    }
 
-}
+};
